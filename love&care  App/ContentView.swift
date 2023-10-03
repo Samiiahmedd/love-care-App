a2ffdbb0aaad7148fba518ce917706e73ddadf63
Sami Ahmed <90336426+Samiiahmedd@users.noreply.github.com>
2023-10-04 01:33:51 +0300

    Initial Commit

---
+++ love&care  App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  love&care  App
+//
+//  Created by Sami Ahmed on 04/10/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ love&care  App/love_care__AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  love_care__AppApp.swift
+//  love&care  App
+//
+//  Created by Sami Ahmed on 04/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct love_care__AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
